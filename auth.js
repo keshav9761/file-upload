1c66eb7b993a98c926c1bb4683c8210e90cda403
Keshav Tomar <keshavtomar53@gmail.com>
2023-10-23 08:40:27 +0530

    file-upload code

--- auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-//http://localhost:5000/signUp
-
-app.post('/signUp', (req, res) => {
-    res.send("i am signUp")
-    // const { userName, password } = req.body;
-})
